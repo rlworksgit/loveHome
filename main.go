c9103cc3117014fca7eafba1351adff6f258b10f
ranliao <752009436@qq.com>
2018-08-21 00:54:40 -0700

    修改重定向

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	_ "loveHome/routers"
+
+	"net/http"
+	"strings"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
-func main() {
-	beego.Run()
+func ignoreStaticPath() {
+
+	//透明static
+
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
 }
 
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+	beego.Debug("request url: ", orpath)
+	//如果请求uri还有api字段,说明是指令应该取消静态资源路径重定向
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
+func main() {
+	ignoreStaticPath()
+	beego.Run()
+}
